456fdf8e67bcd859aa11b10da60c9d2bfd3a068b
Lucas <lucascpinheiro10@gmail.com>
2022-06-07 21:53:55 -0300

    first and probably the last commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## experimenting with deno's blog module/tool
+
+https://deno.land/x/blog@0.3.3
+
+## how to run
+```bash
+# with deno installed and the project cloned run:
+deno run --allow-net --allow-read --allow-env=NODE_DEBUG --watch --no-check main.js --dev
+```
---
+++ main.js
@@ -0,0 +1,11 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  title: "back[end] log",
+  author: "lucas",
+  links: [
+    { title: "Email", url: "mailto:lucascpinheiro10@gmail.com" },
+    { title: "GitHub", url: "https://github.com/lucazpinheiro" },
+  ],
+  background: "#f9f9f9",
+});
